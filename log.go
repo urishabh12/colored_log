2c869c0d029085f3c25e73acfdd516eb7406c4ee
urishabh12 <upadhayayrishabh@gmail.com>
2022-09-22 01:01:59 +0530

    feat: prefix, output

--- log.go
+++ log.go
@@ -19,25 +19,37 @@ const (
 )
 
 type ColoredLogger struct {
-	ldef  *log.Logger
-	lsucc *log.Logger
-	lerr  *log.Logger
-	flags int
-	out   io.Writer
-	mu    sync.Mutex
+	ldef      *log.Logger
+	lsucc     *log.Logger
+	lerr      *log.Logger
+	defColor  string
+	succColor string
+	errColor  string
+	prefix    string
+	flags     int
+	out       io.Writer
+	mu        sync.Mutex
 }
 
-func New(out io.Writer, prefix string, default_color string, success_color string, error_color string, flag int) *ColoredLogger {
+func New(out io.Writer, prefix string, flag int) *ColoredLogger {
+	return NewColored(out, prefix, Blue, Green, Red, flag)
+}
+
+func NewColored(out io.Writer, prefix string, default_color string, success_color string, error_color string, flag int) *ColoredLogger {
 	return &ColoredLogger{
-		ldef:  log.New(os.Stdout, fmt.Sprint(default_color, prefix), flag),
-		lsucc: log.New(os.Stdout, fmt.Sprint(success_color, prefix), flag),
-		lerr:  log.New(os.Stdout, fmt.Sprint(error_color, prefix), flag),
-		flags: flag,
-		out:   out,
+		ldef:      log.New(os.Stdout, fmt.Sprint(default_color, prefix), flag),
+		lsucc:     log.New(os.Stdout, fmt.Sprint(success_color, prefix), flag),
+		lerr:      log.New(os.Stdout, fmt.Sprint(error_color, prefix), flag),
+		defColor:  default_color,
+		succColor: success_color,
+		errColor:  error_color,
+		prefix:    prefix,
+		flags:     flag,
+		out:       out,
 	}
 }
 
-var std_log = New(os.Stdout, "", Blue, Green, Red, log.LstdFlags)
+var std_log = New(os.Stdout, "", log.LstdFlags)
 
 func (l *ColoredLogger) Print(a ...interface{}) {
 	l.mu.Lock()
@@ -126,12 +138,18 @@ func (l *ColoredLogger) SetFlags(i int) {
 }
 
 func (l *ColoredLogger) Prefix() string {
-	return ""
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.prefix
 }
 
-//Placeholder
+// Sets prefix to input
 func (l *ColoredLogger) SetPrefix(s string) {
-	//Do Nothing
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.ldef.SetPrefix(fmt.Sprint(l.defColor, s))
+	l.lsucc.SetPrefix(fmt.Sprint(l.succColor, s))
+	l.lerr.SetPrefix(fmt.Sprint(l.errColor, s))
 }
 
 func (l *ColoredLogger) Writer() io.Writer {
@@ -147,6 +165,12 @@ func (l *ColoredLogger) SetOutput(w io.Writer) {
 	l.ldef.SetOutput(w)
 	l.lsucc.SetOutput(w)
 	l.lerr.SetOutput(w)
+}
+
+func (l *ColoredLogger) Output(calldepth int, s string) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	return l.ldef.Output(calldepth, s)
 }
 
 // Sets outputs of all types of logs to input wirter
@@ -170,7 +194,7 @@ func Prefix() string {
 	return std_log.Prefix()
 }
 
-//Placeholder
+// Sets prefix for all types of logger to input
 func SetPrefix(prefix string) {
 	std_log.SetPrefix(prefix)
 }
@@ -250,4 +274,9 @@ func Successln(a ...interface{}) {
 // Prints in success color
 func Successf(format string, v ...interface{}) {
 	std_log.Successf(format, v...)
+}
+
+// Similar to logger Output()
+func Output(calldepth int, s string) error {
+	return std_log.Output(calldepth, s) // +1 for this frame.
 }
